b74e31b9a6ee25547a296ed95365318382b6e7ce
Abigail Friedman <abigailfriedman@Abigails-MacBook-Air.local>
2021-07-01 15:15:03 -0400

    started JS and finsihed CSS

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # m6-hw9-Friedman_Abigail
+
+# https://www.w3schools.com/html/html_forms.asp
+# https://www.w3schools.com/tags/att_input_autofocus.asp 
+# https://www.freecodecamp.org/news/building-a-weather-app-a3cec42b11fa/
+
--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log ("hello")
+var input = document.getElementsByClassName('type-city')
+var apiKey = "c9e8a1e5219afb02e36d02feb8ea798b";
+var inputVal = input.value;
+var btn = document.getElementsByClassName('button')
+var weatherEl = 
+
+btn.onclick = function() {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`; // make the request
+    .then(function(res) {
+      return res.json() 
+    })
+    .then(function(res) {
+      renderWeather(res.results)
+    })
+  }
+
+  console.log(
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;) // make the request
+    .then(function(res) {
+      return res.json() 
+    })
+  )
