8064353deb398a7e368e708c0dfefc1db858f5f7
Abigail Friedman <abigailfriedman@Abigails-MacBook-Air.local>
2021-07-01 15:32:55 -0400

    js fetch function

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 var input = document.getElementsByClassName('type-city')
 var apiKey = "c9e8a1e5219afb02e36d02feb8ea798b";
+var input = document.getElementsByClassName('type-city')
 var inputVal = input.value;
 var btn = document.getElementsByClassName('button')
 var weatherEl = 
 
 btn.onclick = function() {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`; // make the request
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`) // make the request
     .then(function(res) {
       return res.json() 
     })
@@ -14,9 +15,3 @@ btn.onclick = function() {
     })
   }
 
-  console.log(
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;) // make the request
-    .then(function(res) {
-      return res.json() 
-    })
-  )
