d6fa692e227aa5d464cfacb0f22db60285e1789e
Abigail Friedman <abigailfriedman@Abigails-MacBook-Air.local>
2021-07-03 09:43:04 -0400

    top js fixes

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 var input = document.getElementsByClassName('type-city')
 var apiKey = "c9e8a1e5219afb02e36d02feb8ea798b";
 var input = document.getElementsByClassName('type-city')
-var inputVal = input.value;
+//var inputVal = input.value;
 var btn = document.getElementsByClassName('button')
 var weatherEl = 
 
 btn.onclick = function() {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`) // make the request
-    .then(function(res) {
-      return res.json() 
-    })
-    .then(function(res) {
-      renderWeather(res.results)
-    })
+  var input = document.getElementsByClassName('type-city')
+  //var inputVal = input.value;
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apiKey}&units=metric`) // make the request
+  .then(function(res) {
+    return res.json() 
+  })
+  .then(function(res) {
+  renderWeather(res.results)
+  console.log(res)
+   })
   }
 
