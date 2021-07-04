11ee8d927eeb51b1dd34bbdf4ea7094636efee22
Abigail Friedman <abigailfriedman@Abigails-Air.lan>
2021-07-04 14:56:42 -0400

    Added the rest of the js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var apiKey = "c9e8a1e5219afb02e36d02feb8ea798b";
 var input = document.getElementsByClassName('type-city')
 //var inputVal = input.value;
 var btn = document.getElementsByClassName('button')
-var weatherEl = 
+var weatherEl = document.getElementById('weatherContianer')
 
 btn.onclick = function() {
   var input = document.getElementsByClassName('type-city')
@@ -14,7 +14,54 @@ btn.onclick = function() {
   })
   .then(function(res) {
   renderWeather(res.results)
-  console.log(res)
+  //console.log(res)
    })
+  }
+
+
+   /*
+    Name of the Searched City with Country Code
+    Description of current weather
+    Icon image for current weather conditions
+    Current Temperature
+    Current "feels like" Temperature
+  */
+
+  function renderWeather(weather) {
+    weatherEl.innerHTML = ""
+    weather.forEach(weather => {
+      var newContainer = document.createElement('div')
+      newContainer.classList.add('new-container')
+  
+      var pic = document.createElement('img')
+      pic.src = user.picture.medium
+      pic.style = "float: left; margin-right: 12px;"
+      userContainer.appendChild(pic)
+  
+      var cityName = document.createElement('h3')
+      cityName.textContent = res.name + " " + res.sys.country
+      newContainer.appendChild(cityName)
+  
+      var description = document.createElement('p')
+      cell.style.display = 'block'
+      description.textContent = weather.description
+      userContainer.appendChild(description)
+  
+      var temp = document.createElement('p')
+      temp.style.display = 'block'
+      temp.textContent = weather.temp
+      userContainer.appendChild(temp)
+
+      var feelsLike = document.createElement('p')
+      feelsLike.style.display = 'block'
+      feelsLike.textContent = weather.feelsLike
+      userContainer.appendChild(feelsLike)
+  
+      userContainer.appendChild(
+        document.createElement('hr')
+      )
+  
+      weatherEl.appendChild(userContainer)
+    });
   }
 
