8c01585061b635aea02ea1811bf052d1597f15d5
Abigail Friedman <abigailfriedman@Abigails-MacBook-Air.local>
2021-07-04 22:30:24 -0400

    Got the function to work on select elements

--- README.md
+++ README.md
@@ -3,4 +3,8 @@
 # https://www.w3schools.com/html/html_forms.asp
 # https://www.w3schools.com/tags/att_input_autofocus.asp 
 # https://www.freecodecamp.org/news/building-a-weather-app-a3cec42b11fa/
+# https://www.geeksforgeeks.org/why-we-use-then-method-in-javascript/
+# https://webdesign.tutsplus.com/tutorials/build-a-simple-weather-app-with-vanilla-javascript--cms-33893
+#https://www.youtube.com/watch?v=n4dtwWgRueI
 
+#I had a lot of issues and I am not sure all of my elements work.
--- main.js
+++ main.js
@@ -1,67 +1,35 @@
-var input = document.getElementsByClassName('type-city')
-var apiKey = "c9e8a1e5219afb02e36d02feb8ea798b";
-var input = document.getElementsByClassName('type-city')
-//var inputVal = input.value;
-var btn = document.getElementsByClassName('button')
-var weatherEl = document.getElementById('weatherContianer')
 
-btn.onclick = function() {
-  var input = document.getElementsByClassName('type-city')
-  //var inputVal = input.value;
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apiKey}&units=metric`) // make the request
-  .then(function(res) {
-    return res.json() 
-  })
-  .then(function(res) {
-  renderWeather(res.results)
-  //console.log(res)
-   })
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+  if (evt.keyCode == 13) {
+    getResults(searchbox.value);
   }
+}
 
+function getResults (query) {
+  var cityName = document.getElementsByClassName("search-box")[0].value;
+  var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=c9e8a1e5219afb02e36d02feb8ea798b'
+  
+  fetch(weatherUrl)
+  .then(weather => {
+    return weather.json();
+  }).then(displayResults);
+}
 
-   /*
-    Name of the Searched City with Country Code
-    Description of current weather
-    Icon image for current weather conditions
-    Current Temperature
-    Current "feels like" Temperature
-  */
+function displayResults (weather) {
+  console.log(weather);
+  let city = document.querySelector('.location .city');
+  city.innerText = weather.name, weather.sys.country;
 
-  function renderWeather(weather) {
-    weatherEl.innerHTML = ""
-    weather.forEach(weather => {
-      var newContainer = document.createElement('div')
-      newContainer.classList.add('new-container')
-  
-      var pic = document.createElement('img')
-      pic.src = user.picture.medium
-      pic.style = "float: left; margin-right: 12px;"
-      userContainer.appendChild(pic)
-  
-      var cityName = document.createElement('h3')
-      cityName.textContent = res.name + " " + res.sys.country
-      newContainer.appendChild(cityName)
-  
-      var description = document.createElement('p')
-      cell.style.display = 'block'
-      description.textContent = weather.description
-      userContainer.appendChild(description)
-  
-      var temp = document.createElement('p')
-      temp.style.display = 'block'
-      temp.textContent = weather.temp
-      userContainer.appendChild(temp)
+  let temp = document.querySelector('.current .temp');
+  temp.innerText = Math.round(weather.main.temp);
 
-      var feelsLike = document.createElement('p')
-      feelsLike.style.display = 'block'
-      feelsLike.textContent = weather.feelsLike
-      userContainer.appendChild(feelsLike)
-  
-      userContainer.appendChild(
-        document.createElement('hr')
-      )
-  
-      weatherEl.appendChild(userContainer)
-    });
-  }
+  let feelsLike = document.querySelector('.current .feelsLike');
+  feelsLike.innerText = Math.round(weather.main.feelsLike);
+
+  let weather_el = document.querySelector('.current .description');
+  weather_el.innerText = weather.weather[0].main;
+}
 
